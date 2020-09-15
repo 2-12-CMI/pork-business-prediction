ed3f2c9445eb2d5345ea5b487d4a89bdceb0aaae
제리 <jaryapp@gmail.com>
2020-09-15 18:31:11 +0900

    관리자페이지 환경 세팅

---
+++ admin-page/pages/index.js
@@ -0,0 +1,31 @@
+import '../styles/index.scss'
+
+const Index = () => (
+    <div>
+        <h1>
+            Hello, Next.js
+        </h1>
+    </div>
+);
+
+
+export default Index;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
