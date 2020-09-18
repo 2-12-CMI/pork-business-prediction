4b83710b9fd15f7890cd33a80de938fc8148694f
제리 <jaryapp@gmail.com>
2020-09-18 18:10:12 +0900

    [#8] feat: sidebar

--- admin-page/components/Sidebar.js
+++ admin-page/components/Sidebar.js
@@ -1,21 +1,91 @@
 import Link from "next/link";
 import "../styles/sidebar.scss";
 
-const Sidebar = () => (
-  <div className="sidebar">
-    <Link href="/">
-      <div className="menu">Dashboard</div>
-    </Link>
-    <Link href="/News" className="menu">
-      <div className="menu">News</div>
-    </Link>
-    <Link href="/Price" className="menu">
-      <div className="menu">Price</div>
-    </Link>
-    <Link href="/TradingVolume" className="menu">
-      <div className="menu">Trading Volume</div>
-    </Link>
-  </div>
-);
+const Sidebar = () => {
+  const data = [
+    {
+      name: "",
+      links: [
+        {
+          label: "Dashboard",
+          href: "/",
+        },
+      ],
+    },
+    {
+      name: "RAW DATA",
+      links: [
+        {
+          label: "News",
+          href: "/news",
+        },
+        {
+          label: "Price",
+          href: "/price",
+        },
+        {
+          label: "Trading Volume",
+          href: "/trading-volume",
+        },
+        {
+          label: "Import",
+          href: "/import",
+        },
+        {
+          label: "Export",
+          href: "/export",
+        },
+      ],
+    },
+    {
+      name: "STATISTICS",
+      links: [
+        {
+          label: "Price",
+          href: "/statistics/price",
+        },
+        {
+          label: "Trading Volume",
+          href: "/statistics/trading-volume",
+        },
+        {
+          label: "Intergrated Chart",
+          href: "/statistics/intergrated-chart",
+        },
+      ],
+    },
+    {
+      name: "ANALYSIS",
+      links: [
+        {
+          label: "Topic Modeling",
+          href: "/analysis/topic-modeling",
+        },
+        {
+          label: "Prediction",
+          href: "/analysis/topic-modeling",
+        },
+      ],
+    },
+  ];
+
+  const Components = data.map((category) => {
+    let Category = [];
+    if (category.name)
+      Category.push(<div className="category-name">{category.name}</div>);
+    Category = Category.concat(
+      category.links.map((link) => {
+        return (
+          <Link href={link.href}>
+            <div className="menu">{link.label}</div>
+          </Link>
+        );
+      }),
+    );
+    return Category;
+  });
+
+  return <div className="sidebar">{Components}</div>;
+};
 
 export default Sidebar;
